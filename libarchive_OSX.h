65abc7c589b724b44af6b88f850a61fed20bfbaa
sunmix <sunmix@users.noreply.github.com>
2015-09-19 00:19:56 +0800

    Initial version.

---
+++ libarchive_OSX.h
@@ -0,0 +1,19 @@
+//
+//  libarchive-OSX.h
+//  libarchive-OSX
+//
+//  Created by Ming on 15/9/18.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for libarchive-OSX.
+FOUNDATION_EXPORT double libarchive_OSXVersionNumber;
+
+//! Project version string for libarchive-OSX.
+FOUNDATION_EXPORT const unsigned char libarchive_OSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libarchive_OSX/PublicHeader.h>
+#import <libarchive_iOS/archive.h>
+#import <libarchive_iOS/archive_entry.h>
---
+++ xcodescripts/bootstrap.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+LIBARCHIVE_FILE=libarchive-${LIBARCHIVE_VERSION}.tar.gz
+LIBARCHIVE_FILE_MD5=efad5a503f66329bb9d2f4308b5de98a
+LIBARCHIVE_URL="http://www.libarchive.org/downloads/${LIBARCHIVE_FILE}"
+LIBARCHIVE_DIR=${PROJECT_DIR}/_libarchive_
+LIBARCHIVE_VERSION_FILE=${LIBARCHIVE_DIR}/build/version
+
+if [ -f "${LIBARCHIVE_VERSION_FILE}" ]; then
+  echo "note: Libarchive exists!"
+  if [ "${LIBARCHIVE_VERSION_NUMBER}" != "$( cat ${LIBARCHIVE_VERSION_FILE} )" ]; then
+    echo "warning: Libarchive exists. But its version is NOT ${LIBARCHIVE_VERSION}!
+    Please make sure you know what is going on."
+  fi
+  exit
+fi
+
+cd ${TEMP_ROOT}
+
+if [ ! -f "${LIBARCHIVE_FILE}" ] || [ "${LIBARCHIVE_FILE_MD5}" != "$( md5 -q ${LIBARCHIVE_FILE} )" ]; then
+  [ -f ${LIBARCHIVE_FILE} ] && rm ${LIBARCHIVE_FILE}
+  echo "note: Downloading libarchive from ${LIBARCHIVE_URL}"
+  /usr/bin/curl -# -O ${LIBARCHIVE_URL} && echo "note: Downloaded!"
+  if [ "${LIBARCHIVE_FILE_MD5}" != "$( md5 -q ${LIBARCHIVE_FILE} )" ]; then
+    echo "error: MD5 NOT matches!"
+    exit 1
+  fi
+fi
+
+echo "note: Unarchive ${LIBARCHIVE_FILE}"
+tar xzf ${LIBARCHIVE_FILE}
+
+echo "note: Move libarchive to workspace folder."
+mv libarchive-${LIBARCHIVE_VERSION} ${LIBARCHIVE_DIR}
+
+cd ${LIBARCHIVE_DIR}
+echo "note: Generate config.h"
+./configure --disable-bsdcpio --disable-bsdtar
+
+echo "note: Bootstrap done!"
+exit
