6192a3f618e5041afeebdb54ea99cd7fdf590a9f
sunmix <sunmix@users.noreply.github.com>
2015-09-20 15:06:32 +0800

    Rename both libarchive_OSX and libarchive_iOS to libarchive.

---
+++ libarchive.h
@@ -0,0 +1,17 @@
+//
+//  libarchive.h
+//  libarchive
+//
+//  Created by Ming on 15/9/18.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+#import <libarchive/archive.h>
+#import <libarchive/archive_entry.h>
+
+//! Project version number for libarchive.
+FOUNDATION_EXPORT double libarchiveVersionNumber;
+
+//! Project version string for libarchive.
+FOUNDATION_EXPORT const unsigned char libarchiveVersionString[];
